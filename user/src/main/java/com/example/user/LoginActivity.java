fd7e35daa94bc5e65a5ad1ebff05cf31520b4209
zhangruifei <3482464467@qq.com>
2021-04-19 19:44:34 +0800

    模块代码作业

---
+++ user/src/main/java/com/example/user/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.framework.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
+}
