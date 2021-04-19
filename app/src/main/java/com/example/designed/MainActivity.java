827ec4202ba6df9c076be50eded5db9bc028744b
bryan sun <2802134520@qq.com>
2021-04-19 14:07:04 +0800

    田

--- app/src/main/java/com/example/designed/MainActivity.java
+++ app/src/main/java/com/example/designed/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.designed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.blankj.utilcode.util.LogUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("ertrdssdf", "onCreate: ");
     }
 }
