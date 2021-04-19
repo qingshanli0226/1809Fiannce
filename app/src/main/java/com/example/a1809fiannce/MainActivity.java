4c3c275e74d279cda47286da6c5a0b826565d7e0
weipei <2747160127@qq.com>
2021-04-19 13:57:30 +0800

    第一次修改Activity

--- app/src/main/java/com/example/a1809fiannce/MainActivity.java
+++ app/src/main/java/com/example/a1809fiannce/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a1809fiannce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "zzz", Toast.LENGTH_SHORT).show();
+        Log.e("","");
     }
 }
