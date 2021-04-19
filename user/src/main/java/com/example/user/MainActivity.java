3a6394154bc8feb750a767a9906c6a7a5c6d6633
weipei <2747160127@qq.com>
2021-04-19 19:14:37 +0800

    隐Event-wp

--- user/src/main/java/com/example/user/MainActivity.java
+++ user/src/main/java/com/example/user/MainActivity.java
@@ -1,12 +1,14 @@
-package com.example.a1809fiannce;
+package com.example.user;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initView();
+
+        EventBus.getDefault().register(this);
+
         st.setOnClickListener(view -> {
-            EventBus.getDefault().postSticky("你好");
-            startActivity(new Intent("lg"));
+
         });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe(sticky = true)
+    public void even(String str){
+        Toast.makeText(this, ""+str, Toast.LENGTH_SHORT).show();
     }
 
     private void initView() {
