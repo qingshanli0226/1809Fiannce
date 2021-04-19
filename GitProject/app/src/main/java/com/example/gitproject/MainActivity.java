ce311d1f9d6490f6433ee8b7e22ecec50a09f8cb
zhaoyanbo <3096481245@qq.com>
2021-04-19 19:27:50 +0800

    组件化

--- GitProject/app/src/main/java/com/example/gitproject/MainActivity.java
+++ GitProject/app/src/main/java/com/example/gitproject/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.gitproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
+import com.example.user.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("TAG", "onCreate: ");
+
+        initView();
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+
     }
+
+    private void initView() {
+        skip = (Button) findViewById(R.id.skip);
+    }
+    
 }
