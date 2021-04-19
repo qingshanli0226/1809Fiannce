368fd0ad67d3ceab148bb7d118722daa7377cd65
ZhangXian <2118587626@qq.com>
2021-04-19 20:23:55 +0800

    Arouter

--- pay/src/main/java/com/example/pay/PayActivity.java
+++ pay/src/main/java/com/example/pay/PayActivity.java
@@ -6,14 +6,20 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 
 @Route(path = "/zx/zx1")
 public class PayActivity extends AppCompatActivity {
+    @Autowired(name = "name")
+    String name;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_pay);
-        Toast.makeText(this, "111", Toast.LENGTH_SHORT).show();
+        //加个方法
+        ARouter.getInstance().inject(this);
+        Toast.makeText(this, ""+name, Toast.LENGTH_SHORT).show();
     }
 }
