a0135463affd8c0fcecc14fdea8a4e1624e2dd91
ZhangXian <2118587626@qq.com>
2021-04-19 13:41:05 +0800

    上传项目

---
+++ app/src/main/java/com/example/a1809fiannce/App.java
@@ -0,0 +1,10 @@
+package com.example.a1809fiannce;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/example/a1809fiannce/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a1809fiannce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("zx", "onCreate: 1111");
+    }
+}
