763b77647978d8415d3c5ca7e62d44f8e3708c8b
zhaoyonghui <2513295133@qq.com>
2021-04-19 13:26:21 +0800

    添加一个app

---
+++ app/src/main/java/com/example/a1809zg/App.java
@@ -0,0 +1,10 @@
+package com.example.a1809zg;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
