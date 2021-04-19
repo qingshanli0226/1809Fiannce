4bcf69e2d22e4d5d52886f6b4215a61b4c93ce11
hanqingyu <1922594647@qq.com>
2021-04-19 14:25:02 +0800

    xiugai

---
+++ app/src/main/java/com/finance/zg6/FinanceApp.java
@@ -0,0 +1,11 @@
+package com.finance.zg6;
+
+import android.app.Application;
+
+public class FinanceApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
