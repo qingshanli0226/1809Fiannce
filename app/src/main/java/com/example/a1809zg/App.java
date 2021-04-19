50371aff29f1d245db814310725212b59612eb65
zhaoyonghui <2513295133@qq.com>
2021-04-19 14:09:13 +0800

    提交新的

--- app/src/main/java/com/example/a1809zg/App.java
+++ app/src/main/java/com/example/a1809zg/App.java
@@ -1,10 +1,12 @@
 package com.example.a1809zg;
 
 import android.app.Application;
+import android.util.Log;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.i("666", "onCreate: ");
     }
 }
