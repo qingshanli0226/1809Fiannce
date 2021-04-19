f8564a5c1a0543a933de4a55f3e55c62cbf0f04d
weipei <2747160127@qq.com>
2021-04-19 14:39:37 +0800

    赵岩博很正经

--- app/src/main/java/com/example/a1809fiannce/MainActivity.java
+++ app/src/main/java/com/example/a1809fiannce/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "zzz", Toast.LENGTH_SHORT).show();
-        Log.e("","");
+        Log.e("","不正经的赵岩博");
+
+
+
     }
 }
