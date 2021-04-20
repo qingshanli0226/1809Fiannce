5c1483644f9112237f357f722211a05a7c6acf10
liyifan <3202327205@qq.com>
2021-04-20 18:36:59 +0800

    练习

---
+++ app/src/main/java/com/example/a1809fiannce/welcome/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.a1809fiannce.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.example.a1809fiannce.R;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+}
