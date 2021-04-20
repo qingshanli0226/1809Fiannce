cadbce3ce919d7695da1ecf8c39e92d2347148b5
ZhangXian <2118587626@qq.com>
2021-04-20 18:41:58 +0800

    添加

---
+++ app/src/main/java/com/example/a1809fiannce/CallBack.java
@@ -0,0 +1,12 @@
+package com.example.a1809fiannce;
+
+import com.example.formwork.model.HomeBean;
+import com.example.formwork.model.UpdateBean;
+
+public interface CallBack {
+
+    void HomeData(HomeBean homeBean);
+
+    void UpdateData(UpdateBean updateBean);
+
+}
---
+++ app/src/main/java/com/example/a1809fiannce/update/UpdatePresenter.java
@@ -0,0 +1,5 @@
+package com.example.a1809fiannce.update;
+
+public class UpdatePresenter {
+    
+}
