e3052cdf76e1b426434fc730af312519de1c709b
zhaoziyu <2774410792@qq.com>
2021-04-20 20:43:21 +0800

    完成

--- app/src/main/java/com/Fiannce/myapplication/MainActivity.java
+++ app/src/main/java/com/Fiannce/myapplication/MainActivity.java
@@ -11,16 +11,20 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.fiannce.framework.BaseActivity;
 import com.fiannce.framework.manager.CacheManager;
 import com.fiannce.net.mode.HomeBean;
+import com.youth.banner.Banner;
+
 @Route(path="/main/MainActivity")
 public class MainActivity extends BaseActivity {
 
 
     private android.widget.TextView progressTv;
+    private com.youth.banner.Banner banner;
 
     @Override
     protected void initView() {
 
         progressTv = (TextView) findViewById(R.id.progressTv);
+        banner = (Banner) findViewById(R.id.banner);
     }
 
     @Override
@@ -32,6 +36,8 @@ public class MainActivity extends BaseActivity {
     protected void initData() {
         HomeBean homeBean = CacheManager.getInstance().getHomeBean();
         progressTv.setText("首页"+homeBean.toString());
+        
+
     }
 
     @Override
