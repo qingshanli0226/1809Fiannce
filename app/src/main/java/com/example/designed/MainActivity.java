57ca29bd98fb4b1832b2fa6c57e7da0614599b43
bryan sun <2802134520@qq.com>
2021-04-20 09:11:30 +0800

    交

--- app/src/main/java/com/example/designed/MainActivity.java
+++ app/src/main/java/com/example/designed/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("ertrdssdf", "onCreate: ");
+        Log.i("fghjiuytfvbnjk", "onCreate: ");
 
     }
 }
