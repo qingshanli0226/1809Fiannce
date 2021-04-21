facbe88d2290e4e7c6627ff9cfab7f988f2bcfa7
hanqingyu <1922594647@qq.com>
2021-04-21 09:28:48 +0800

    a

--- user/src/main/java/com/finance/user/UserActivity.java
+++ user/src/main/java/com/finance/user/UserActivity.java
@@ -1,10 +1,5 @@
 package com.finance.user;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
