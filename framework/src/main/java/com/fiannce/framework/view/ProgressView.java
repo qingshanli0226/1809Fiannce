33cb9c41970245b8c268d02438f2220023ee2b6a
zhaoziyu <2774410792@qq.com>
2021-04-23 20:34:17 +0800

    完工

--- framework/src/main/java/com/fiannce/framework/view/ProgressView.java
+++ framework/src/main/java/com/fiannce/framework/view/ProgressView.java
@@ -1,26 +1,182 @@
 package com.fiannce.framework.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.fiannce.framework.R;
 
 public class ProgressView extends View {
 
+    private Paint paint;
+    private final int START_ANGLE = 0;
+    private final int STEP_ANGLE = 1;
+    private int offsetAngle;
+    private int progressAngle = 0;
+    private int progressViewWidth;
+    private int progressViewHeight;
+    private int progess;
+    private final int CIRCLE_MARGIN = 5;
+    private int textColor;
+    private int circleWith;
+    private int backgroundId;
+
     public ProgressView(Context context) {
         super(context);
+        init(context,null,0);
     }
 
-    public ProgressView(Context context, @Nullable AttributeSet attrs) {
+    public ProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context,attrs,0);
     }
 
-    public ProgressView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public ProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context,attrs,defStyleAttr);
     }
 
-    public ProgressView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        paint = new Paint();
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.ProgressView);
+        textColor = typedArray.getColor(R.styleable.ProgressView_textColor,Color.BLACK);
+        circleWith = typedArray.getInt(R.styleable.ProgressView_circleWith,5);
+        backgroundId = typedArray.getResourceId(R.styleable.ProgressView_background,0);
+
+        typedArray.recycle();
     }
+
+
+    public void saledProgress(int progress,boolean isAnimal) {
+        offsetAngle = (progress * 360)/100;
+        if (isAnimal) {
+            progressAngle = 0;
+        } else {
+            progressAngle = offsetAngle;
+        }
+
+        if (progressAngle<=offsetAngle) {
+            invalidate();
+            handler.sendEmptyMessageDelayed(1,10);
+        }
+
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            progressAngle = progressAngle + STEP_ANGLE;
+            if (progressAngle<=offsetAngle) {
+                invalidate();
+                handler.sendEmptyMessageDelayed(1,10);
+            }
+        }
+    };
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        event.getY();event.getRawY();
+        event.getX();event.getRawX();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    public void setTextColor(int color) {
+        this.textColor = color;
+        invalidate();
+    }
+
+    public void setCircleWith(int width) {
+        this.circleWith = width;
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int realWidth,realHeitht;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        if (widthMode == MeasureSpec.AT_MOST) {
+
+            realWidth = 50;
+        } else {
+            realWidth = MeasureSpec.getSize(widthMeasureSpec);
+        }
+        if (heightMode == MeasureSpec.AT_MOST) {
+            realHeitht = 50;
+        } else {
+            realHeitht = MeasureSpec.getSize(heightMeasureSpec);
+        }
+
+        setMeasuredDimension(realWidth,realHeitht);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),backgroundId);
+        Matrix matrix = new Matrix();
+        canvas.drawBitmap(bitmap,matrix,paint);
+        progressViewWidth = getMeasuredWidth();
+        progressViewHeight = getMeasuredHeight();
+        int centerX = progressViewWidth/2;
+        int centerY = progressViewHeight/2;
+
+        int radius = (progressViewWidth<progressViewHeight?progressViewWidth/2:progressViewHeight/2)-CIRCLE_MARGIN;
+        paint.setColor(Color.BLUE);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(circleWith);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(centerX,centerY,radius,paint);
+
+
+
+        RectF rectF = new RectF(progressViewWidth/2-radius,progressViewHeight/2-radius,progressViewWidth/2+radius,progressViewHeight/2+radius);
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(circleWith);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(rectF,START_ANGLE,progressAngle,false,paint);
+
+
+        Rect rect = new Rect();
+        paint.setColor(textColor);
+        paint.setTextSize(30);
+        paint.setStrokeWidth(2);
+        String content = (progressAngle*100)/360+"%";
+
+        paint.getTextBounds(content,0,content.length(),rect);
+        canvas.drawText(content,progressViewWidth/2-rect.width()/2,progressViewHeight/2+rect.height()/2,paint);
+    }
+
+    public void destry() {
+        handler.removeCallbacksAndMessages(null);
+    }
+
 }
--- framework/src/main/java/com/fiannce/framework/view/ToolBar.java
+++ framework/src/main/java/com/fiannce/framework/view/ToolBar.java
@@ -1,23 +1,99 @@
 package com.fiannce.framework.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.fiannce.framework.R;
 
 public class ToolBar extends RelativeLayout {
+    private TextView titleTv;
+    private LinearLayout rightArea;
+    private ImageView leftImg;
+    private ImageView rightImg;
+    private TextView rightTv;
+    private boolean rightAreaIsShow,leftIsShow;
+    private String titleText;
+    private int rightImgId,leftImgId;
+
+    private IToolbarListener iToolbarListener;
+
     public ToolBar(Context context) {
         super(context);
+        init(context,null,0);
     }
 
     public ToolBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context,attrs,0);
     }
 
     public ToolBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context,attrs,defStyleAttr);
     }
 
-    public ToolBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    public void init(Context context, AttributeSet attrs, int defStyleAttr) {
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ToolBar);
+        titleText = typedArray.getString(R.styleable.ToolBar_titleText);
+        leftImgId = typedArray.getResourceId(R.styleable.ToolBar_leftImg,0);
+        rightImgId = typedArray.getResourceId(R.styleable.ToolBar_rightImage,0);
+        rightAreaIsShow = typedArray.getBoolean(R.styleable.ToolBar_rightIsShow,false);
+        leftIsShow = typedArray.getBoolean(R.styleable.ToolBar_leftIsShow,false);
+        typedArray.recycle();
+
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.view_toolbar,this);
+
+        titleTv = findViewById(R.id.titleTv);
+        rightArea = findViewById(R.id.rightArea);
+        leftImg = findViewById(R.id.leftImg);
+        rightImg = findViewById(R.id.rightImg);
+        rightTv = findViewById(R.id.rightTv);
+
+        titleTv.setText(titleText);
+        if (rightAreaIsShow && rightImgId!=0) {
+            rightImg.setImageResource(rightImgId);
+        }
+        if (leftIsShow && leftImgId!=0) {
+            leftImg.setImageResource(leftImgId);
+        }
+
+        leftImg.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (iToolbarListener!=null) {
+                    iToolbarListener.onLeftClick();
+                }
+            }
+        });
+        rightImg.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (iToolbarListener!=null) {
+                    iToolbarListener.onRightImgClick();
+                }
+            }
+        });
+
+
+    }
+
+    public void setToolbarListener(IToolbarListener iToolbarListener) {
+        this.iToolbarListener = iToolbarListener;
+    }
+
+    public static interface IToolbarListener {
+        void onLeftClick();
+        void onRightImgClick();
+        void onRightTvClick();
     }
 }
