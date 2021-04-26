e52f6b39e900603c7d745e2a447958f80e3a6848
liqingshan <liqingshan@sina.cn>
2021-04-26 11:14:18 +0800

    comment add

--- app/src/main/java/com/fiannce/bawei/a1809fiannce/event/HomeAdapter.java
+++ app/src/main/java/com/fiannce/bawei/a1809fiannce/event/HomeAdapter.java
@@ -46,6 +46,7 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
         holder.delBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //点击删除按钮就隐藏该删除按钮
                 lastItemView.scrollTo(0,0);
                 lastItemView = null;
                 lastPosition = -1;
@@ -58,28 +59,25 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         lastX = (int) event.getRawX();
-                        //holder.itemView.getParent().requestDisallowInterceptTouchEvent(true);
-                        return true;
+                        //请求外部不要拦截事件，之后的滑动事件就可以收到了
+                        holder.itemView.getParent().requestDisallowInterceptTouchEvent(true);
+                        return true;//返回true代表的是消费该事件，如果返回false系统就不会把之后的事件返回了
                     case MotionEvent.ACTION_MOVE:
-                        if (lastX>500 && event.getRawX()<lastX) {
-                            //holder.itemView.getParent().requestDisallowInterceptTouchEvent(true);
-                            if (lastItemView!=null&&position!=lastPosition) {
+                        if (lastX>500 && event.getRawX()<lastX) {//判断如果是在右侧滑动，且是向左滑动，就请求外部不要拦截事件，否则就允许外部拦截事件
+                            holder.itemView.getParent().requestDisallowInterceptTouchEvent(true);
+                            if (lastItemView!=null&&position!=lastPosition) {//如果之前有删除按钮显示就先隐藏掉
                                 lastItemView.scrollTo(0,0);
                             }
                             lastPosition = position;
                             lastItemView = holder.itemView;
-                            scrollDiffX = -(int) ((event.getRawX()-lastX));
-                            holder.itemView.scrollTo(scrollDiffX,0);
-                            return true;
+                            scrollDiffX = -(int) ((event.getRawX()-lastX));//记住手指在X方向的滑动距离
+                            holder.itemView.scrollTo(scrollDiffX,0);//让ItemView向做滑动
+                            return true;//消费该事件
                         } else {
-                            //holder.itemView.getParent().requestDisallowInterceptTouchEvent(false);
+                            holder.itemView.getParent().requestDisallowInterceptTouchEvent(false);
                         }
-
-                        break;
-
                 }
-
-                return false;
+                return false;//如果不是我们关注的事件，就不消费了
             }
         });
     }
